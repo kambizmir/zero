8e81efc9759b3c8cb408b5617aa8598b97d17700
Kambiz Mir <kmir@paciolan.com>
2018-12-23 20:33:58 -0800

    Change create instance dialog text. Add text box

--- src/components/shell/NewInstanceDialog.js
+++ src/components/shell/NewInstanceDialog.js
@@ -4,7 +4,6 @@ import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
 export default class FormDialog extends React.Component {
@@ -28,27 +27,31 @@ export default class FormDialog extends React.Component {
           onClose={this.handleClose}
           aria-labelledby="form-dialog-title"
         >
-          <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+          <DialogTitle id="form-dialog-title">Create Instance</DialogTitle>
           <DialogContent>
-            <DialogContentText>
-              To subscribe to this website, please enter your email address here. We will send
-              updates occasionally.
-            </DialogContentText>
+ 
             <TextField
               autoFocus
               margin="dense"
-              id="name"
-              label="Email Address"
-              type="email"
+              id="id"
+              label="Id"              
               fullWidth
             />
+
+            <TextField              
+              margin="dense"
+              id="name"
+              label="Name"              
+              fullWidth
+            />
+
           </DialogContent>
           <DialogActions>
             <Button onClick={this.handleClose} color="primary">
               Cancel
             </Button>
             <Button onClick={this.handleClose} color="primary">
-              Subscribe
+              Create
             </Button>
           </DialogActions>
         </Dialog>
